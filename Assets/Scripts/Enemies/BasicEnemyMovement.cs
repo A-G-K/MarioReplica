f775ee034d273b7c8bf96aeb73a7a151217d23f3
A-G-K <agk8097@gmail.com>
2021-03-06 01:31:00 +1100

    Add Koopa with animation

--- Assets/Scripts/Enemies/BasicEnemyMovement.cs
+++ Assets/Scripts/Enemies/BasicEnemyMovement.cs
@@ -6,14 +6,33 @@ using UnityEngine;
 public class BasicEnemyMovement : MonoBehaviour
 {
     public float moveSpeed = 1f;
-    public bool isMovingLeft = true;
+    [SerializeField] private bool isMovingLeft = true;
+    [SerializeField] private Animator spriteAnimator;
     
     private Rigidbody2D rb;
     private ContactPoint2D[] contactCache = new ContactPoint2D[10];
+
+    public bool IsMovingLeft
+    {
+        get { return isMovingLeft; }
+        set
+        {
+            isMovingLeft = value;
+            
+            if (spriteAnimator != null)
+                spriteAnimator.SetBool("IsMovingLeft", value);
+        }
+    }
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        // We need to do this, just in case we have an animator to setup
+        IsMovingLeft = isMovingLeft;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -32,21 +51,25 @@ public class BasicEnemyMovement : MonoBehaviour
 
     private void TryMoveOpposite(Collision2D other)
     {
-        bool shouldFlip = true;
         int contactCount = other.GetContacts(contactCache);
 
         for (int i = 0; i < contactCount; i++)
         {
-            if (contactCache[i].normal.y > 0.5f)
+            Debug.Log($"{gameObject.name} with normal {contactCache[i].normal}");
+            
+            if (contactCache[i].normal.y < 0.5f)
             {
-                shouldFlip = false;
+                if (contactCache[i].normal.x == 1f)
+                {
+                    IsMovingLeft = false;
+                }
+                else if (contactCache[i].normal.x == -1f)
+                {
+                    IsMovingLeft = true;
+                }
+                
                 break;
             }
-        }
-
-        if (shouldFlip)
-        {
-            isMovingLeft = !isMovingLeft;
         }
     }
 }
