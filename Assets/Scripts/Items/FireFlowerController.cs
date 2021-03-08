edff2ce84bd9850d1e25d666f2c15b9f75d5ac73
indoo8 <65959029+indoo8@users.noreply.github.com>
2021-03-09 00:53:54 +1100

    collisions, killzone, camera collider
    created red mushroom and fire flower prefabs (only collider/script) and have working collisions/implementation with player. (used same method used on lifemushroom and coin items)
    
    added killzones in pitfall traps.
    
    re-added collider on camera so you can't walk backwards (won't collide with enemies)
    
    dying from player controller script or killzones will trigger loselife method in lives manager
    
    added bigger collider on player prefab (needs to be toggled in playercontroller script)

---
+++ Assets/Scripts/Items/FireFlowerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlowerController : MonoBehaviour
+{
+
+    private PlayerController pController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pController.IncreaseMarioState();
+            pController.IncreaseMarioState();
+            Destroy(gameObject);
+        }
+    }
+}
