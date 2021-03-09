1c37a274d1d09fe0b6b62d0918907d8a33fad1e6
Thalia Johnson <shaymin43@gmail.com>
2021-03-09 21:20:59 +1100

    Added flag pole ending

---
+++ Assets/Scripts/Blocks/FlagController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    bool movingPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(movingPlayer == true)
+        {
+            rigidBody.velocity = new Vector2(2.0f, rigidBody.velocity.y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerMovement playerMovement = collision.gameObject.GetComponentInParent<PlayerMovement>();
+            rigidBody = collision.gameObject.GetComponentInParent<Rigidbody2D>();
+            StartCoroutine(MovingDownwards());
+            playerMovement.canMove = false;
+        }
+    }
+
+    private IEnumerator MovingDownwards()
+    {
+        rigidBody.velocity = new Vector2(0f, -2f);
+        yield return new WaitForSeconds(1f);
+        movingPlayer = true;
+
+    }
+}
---
+++ Assets/Scripts/Utils/EndGameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameController : MonoBehaviour
+{
+    UIManager uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject managers = GameObject.FindGameObjectWithTag("Managers");
+        uiManager = managers.GetComponent<UIManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //uiManager.end();
+    }
+}
